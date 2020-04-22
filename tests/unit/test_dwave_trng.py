c14adaf7c172dd8471482255badf15e3354b6b36
Harald Heckmann <harald.heckmann93@web.de>
2020-04-22 17:23:06 +0200

    Completely initialized project

---
+++ tests/unit/test_dwave_trng.py
@@ -0,0 +1,8 @@
+import pytest  # noqa: F401
+
+import qpu.dwave.cryptography.trng as dwave_trng  # noqa: F401
+
+
+def test_random_deviation():
+    # TODO: implement
+    assert(42 == 42)
---
+++ tests/unit/test_ibm_trng.py
@@ -0,0 +1,8 @@
+import pytest  # noqa: F401
+
+import qpu.ibm.cryptography.trng as ibm_trng  # noqa: F401
+
+
+def test_random_deviation():
+    # TODO: implement
+    assert(42 == 42)
